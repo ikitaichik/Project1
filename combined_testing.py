47c6d4719cd33c1116e983a627c1d64b409b6e77
ikitaichik <ikitaichik@liveperson.com>
2021-02-02 17:17:17 +0200

    Updated #3 - separation of frontend and backend

--- combined_testing.py
+++ combined_testing.py
@@ -31,4 +31,7 @@ elif args.test == "backend":
     id = db_connector.get_ids()
     backend_testing.get(id)
 elif args.test == "frontend":
-    frontend_testing.frontend_test(1)
+    if "Itzik" == frontend_testing.frontend_test(id):
+        print("Test succeeded")
+    else:
+        raise Exception
